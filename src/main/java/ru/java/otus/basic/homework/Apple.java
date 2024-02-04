2e299936c69d9898519c979f258dddc243ddf45f
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-02-04 14:39:57 +0800

    homework

--- src/main/java/ru/java/otus/basic/homework/Apple.java
+++ src/main/java/ru/java/otus/basic/homework/Apple.java
@@ -1,3 +1,7 @@
 package ru.java.otus.basic.homework;
 
 public class Apple extends Fruit{
+    public Apple(int weight) {
+        super(weight);
+    }
+}
--- src/main/java/ru/java/otus/basic/homework/Box.java
+++ src/main/java/ru/java/otus/basic/homework/Box.java
@@ -4,3 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Box<T extends Fruit> {
+    private List<T> fruits;
+
+    public Box() {
+        this.fruits = new ArrayList<>();
+    }
+
+    public void addFruit(T... fruit) {
+        T[] array = fruit;
+        for (T f : array) {
+            fruits.add(f);
+        }
+        System.out.println("Фрукты в коробке. Всего фруктов: " + fruits.size() + ". Вес: " + weight());
+    }
+
+    public int weight() {
+        int a = 0;
+        for (T fruit : fruits) {
+            a += fruit.getWeight();
+        }
+        return a;
+    }
+
+    public boolean comparisonBox(Box<?> box) {
+        return this.weight() == box.weight();
+    }
+
+    public void boxInBox(Box<? super T> box){
+        if (box == this){
+            return;
+        }
+        box.fruits.addAll(this.fruits);
+        this.fruits.clear();
+        System.out.println("Фрукты в коробке. Всего фруктов: " + box.fruits.size() + ". Вес: " + box.weight());
+    }
+}
